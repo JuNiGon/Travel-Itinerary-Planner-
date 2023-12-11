c83b9f25e2c00e6d402e6dcae0c53b080fcd948b
Julia Goncalves <jugoncalves.ds@gmail.com>
2023-12-11 01:14:58 -0300

    Projeto completo

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ app = FastAPI()
 
 # Caminho para os arquivos JSON que armazenam os dados
 
+INFORMACOES_DESTINOS_FILE = "informacoes_destinos.json"
 USERS_FILE = "usuarios.json"
 ITINERARIES_FILE = "roteiros.json"
 
@@ -78,27 +79,59 @@ def adicionar_roteiro(dados_roteiro: dict):
     salvar_roteiros(roteiros)
     return {"message": "Roteiro adicionado com sucesso!"}
 
-# Rota para buscar roteiros pelo ID
-@app.get("/itineraries/search-itineraries/{id_roteiro}")
-def buscar_roteiro(id_roteiro: str):
+# Rota para buscar roteiros pelo nome cidade e retornar aquele comm maior nota
+@app.get("/itineraries/search-itineraries/{nome_da_cidade}")
+def buscar_roteiro(nome_da_cidade: str):
     roteiros = carregar_roteiros()
+    roteiros_cidade = []
     for roteiro in roteiros["roteiros"]:
-        if roteiro["dados_roteiro"]["id_roteiro"] == id_roteiro:
-            
-            return roteiro
+        if roteiro["dados_roteiro"]["nome_da_cidade"] == nome_da_cidade:
+            nome_cidade = roteiro["dados_roteiro"]["nome_da_cidade"]
+            roteiros_cidade = [r for r in roteiros["roteiros"] if r["dados_roteiro"]["nome_da_cidade"] == nome_cidade]
+            if not roteiros_cidade:
+                raise HTTPException(status_code=404, detail="Roteiro não encontrado")  
+            # Encontrar o roteiro com a maior nota
+            roteiro_maior_nota = max(roteiros_cidade, key=lambda x: x["dados_roteiro"]["nota"])
+            return roteiro_maior_nota
     raise HTTPException(status_code=404, detail="Roteiro não encontrado")
 
 
-# MAPS
-# Retorna coordenadas a partir de uma cidade
-@app.get("/maps/coordinates/{cidade}")
-def get_coordenadas(cidade: str):
+@app.get("/maps/coordinates/")
+def get_coordenadas(cidade_origem: str, cidade_destino: str):
     try:
-        coordenadas = get_coordinates(cidade)
-        return {"cidade": cidade, "coordenadas": {"latitude": coordenadas[0], "longitude": coordenadas[1]}}
+        coordenadas_origem = get_coordinates(cidade_origem)
+        coordenadas_destino = get_coordinates(cidade_destino)
+        return {
+            "cidades": [
+                {"cidade_origem": cidade_origem, "coordenadas": {"latitude": coordenadas_origem[0], "longitude": coordenadas_origem[1]}},
+                {"cidade_destino": cidade_destino, "coordenadas": {"latitude": coordenadas_destino[0], "longitude": coordenadas_destino[1]}}
+            ]   
+        }
     except HTTPException as e:
         raise e
 
+
+
+
+
+
+# função para obter recomendações personalizadas com base nas preferências do usuário
+@app.get("/user/personalized-recommendations/{user_id}")
+async def personalized_recommendations(user_id: str):
+    users = read_users()
+    user = get_user_by_id(user_id, users)
+    if user is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+    cidade_origem_usuario = user["dados_voo"]["origem"]
+    # Verificar se existem roteiros na mesma cidade
+    roteiros = carregar_roteiros()["roteiros"]
+    roteiros_na_mesma_cidade = [r for r in roteiros if r["dados_roteiro"]["nome_da_cidade"] == cidade_origem_usuario]
+    if roteiros_na_mesma_cidade:
+        return {"recomendacoes": roteiros_na_mesma_cidade}
+    else:
+        # Se não houver roteiros na mesma cidade, retornar o roteiro com a maior nota
+        roteiro_maior_nota = max(roteiros, key=lambda x: x["dados_roteiro"]["nota"])
+        return {"recomendacoes": [roteiro_maior_nota]}
 
 # USERS
 # Ver usuários
@@ -106,16 +139,26 @@ def get_coordenadas(cidade: str):
 async def get_users():
     return read_users()
 
-# Adicionar usuário
 @app.post("/user/add")
 async def add_user(user_data: dict):
     user_id = generate_user_id()
+
+    # Adicionando os novos campos e valores
+    user_data["dados_voo"]["valor_voo"] = user_data["dados_voo"].get("valor_voo", 0)
+    user_data["dados_hotel"]["valor_hotel"] = user_data["dados_hotel"].get("valor_hotel", 0)
+    user_data["dados_roteiro"]["valor_roteiro"] = user_data["dados_roteiro"].get("valor_roteiro", 0)
+    user_data["dados_do_usuario"]["valor_total"] = (
+    user_data["dados_voo"]["valor_voo"]
+    + user_data["dados_hotel"]["valor_hotel"]
+    + user_data["dados_roteiro"]["valor_roteiro"]
+)
+
     user_data["id_usuario"] = user_id
 
     users = read_users()
     users.append(user_data)
     write_users(users)
-    
+
     return {"message": "Usuário adicionado com sucesso!", "id_usuario": user_id}
 
 # Editar informações do usuário
@@ -150,8 +193,65 @@ async def delete_user(user_id: str):
         raise HTTPException(status_code=404, detail="Usuário não encontrado")
 
 
+@app.post("/user/copy-itinerary/{id_usuario_1}/{id_usuario_2}")
+async def copy_itinerary(id_usuario_1: str, id_usuario_2: str):
+    users = read_users()
 
-# RECOMENDATION
-# Recomendation methods
-# @app.get("/recomendation/destination/{cidade}")
-# def get_coordenadas(cidade: str):
+    usuario_1 = get_user_by_id(id_usuario_1, users)
+    usuario_2 = get_user_by_id(id_usuario_2, users)
+
+    if usuario_1 is None or usuario_2 is None:
+        raise HTTPException(status_code=404, detail="Usuário não encontrado")
+
+    # Copiar itinerário do usuário 2 para o usuário 1
+    usuario_1["dados_voo"] = usuario_2["dados_voo"]
+    usuario_1["dados_hotel"] = usuario_2["dados_hotel"]
+    usuario_1["dados_roteiro"] = usuario_2["dados_roteiro"]
+
+    # Atualizar o valor total com base nos novos valores
+    usuario_1["dados_do_usuario"]["valor_total"] = (
+        usuario_1["dados_voo"]["valor_voo"]
+        + usuario_1["dados_hotel"]["valor_hotel"]
+        + usuario_1["dados_roteiro"]["valor_roteiro"]
+    )
+
+    # Salvar as alterações
+    write_users(users)
+
+    return {"message": "Itinerário copiado com sucesso"}
+
+
+
+
+
+
+
+
+
+def obter_informacoes_cidade(nome_cidade):
+    try:
+        # Carregar dados do arquivo informacoes_destinos.json
+        with open(INFORMACOES_DESTINOS_FILE, 'r', encoding='utf-8') as file:
+            destinos = json.load(file)
+
+        # Buscar informações da cidade no novo formato JSON
+        cidade_info = next((destino for destino in destinos["destinos"] if destino["cidade"] == nome_cidade), None)
+
+        if cidade_info is None:
+            raise HTTPException(status_code=404, detail=f"Informações não encontradas para a cidade '{nome_cidade}'")
+
+        # Retornar as informações
+        return {'informacoes_cidade': cidade_info}
+
+    except FileNotFoundError:
+        raise HTTPException(status_code=500, detail="Arquivo de dados não encontrado")
+
+
+
+@app.get("/cidade/{nome_cidade}")
+def get_informacoes_cidade(nome_cidade: str):
+    try:
+        informacoes_cidade = obter_informacoes_cidade(nome_cidade)
+        return informacoes_cidade
+    except HTTPException as e:
+        return {"Informação não encontrada": str(e)}
